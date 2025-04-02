0c7943d74a54cc80c896598eb8d5106fa4cbd7dd
Laura Eliosa <laura@EliosaMacMini.local>
2025-04-01 20:38:13 -0400

    Update todo list components and add TodoListItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
-
+import { useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
-
+  const [newTodo, setNewTodo] = useState('');
   return (
-   <div>
-    <h1>My Todos</h1>
-    <TodoForm />
-    <TodoList></TodoList>
-    
-   </div>
-  )
+    <div>
+      <h1>My Todos</h1>
+      <TodoForm />
+      <p>{newTodo}</p>
+      <TodoList></TodoList>
+    </div>
+  );
 }
 
-export default App
+export default App;
