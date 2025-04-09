98c4b90067bc8a4337b41d1b04cf472d5394d198
Laura Eliosa <laura@EliosaMacMini.local>
2025-04-08 23:27:58 -0400

    updated TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,17 +2,10 @@ import TodoListItem from './TodoListItem';
 
 function TodoList({ todoList }) {
   return (
-    // <ul>
-    //   {todoList.map((todo) => (
-    //     <TodoListItem key={todo.id} todo={todo} />
-    //   ))}
-    // </ul>
-
     <ul>
-      {todoList.map((todo) => {
-        console.log(todo);
-        return <TodoListItem key={todo.id} todo={todo} />;
-      })}
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
     </ul>
   );
 }
